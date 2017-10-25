09e146a408a3fab0054cbb241d3f023aa6775aa0
刘浩 <v2top1lyle@gmail.com>
2017-10-25 14:31:13 +0800

    网络请求类

---
+++ MTTNetworkManger/MTTNetworkManger/Sources/MTTRequest.h
@@ -0,0 +1,13 @@
+//
+//  MTTRequest.h
+//  MTTNetworkManger
+//
+//  Created by 刘浩 on 2017/10/25.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTTRequest : NSObject
+
+@end
---
+++ MTTNetworkManger/MTTNetworkManger/Sources/MTTRequest.m
@@ -0,0 +1,13 @@
+//
+//  MTTRequest.m
+//  MTTNetworkManger
+//
+//  Created by 刘浩 on 2017/10/25.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+#import "MTTRequest.h"
+
+@implementation MTTRequest
+
+@end
