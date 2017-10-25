7d6c2298698b9022f2648f2c6224045fbdb275d7
刘浩 <v2top1lyle@gmail.com>
2017-10-25 13:57:58 +0800

    create a singleView application

---
+++ MTTNetworkManger/MTTNetworkManger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTTNetworkManger
+//
+//  Created by 刘浩 on 2017/10/25.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
