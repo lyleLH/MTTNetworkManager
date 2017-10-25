23f0e4f41ebdb54be4dc21e2c5ba4c47aede7e4e
刘浩 <v2top1lyle@gmail.com>
2017-10-25 14:30:47 +0800

    请求单例的配置文件

---
+++ MTTNetworkManger/MTTNetworkManger/Sources/MTTNetworkManagerConfig.h
@@ -0,0 +1,17 @@
+//
+//  MTTNetworkManagerConfig.h
+//  MTTNetworkManger
+//
+//  Created by 刘浩 on 2017/10/25.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static double kTimeoutInterval = 5.0f;
+
+static NSString *kDefaultErrorTips = @"网络异常，请检查网络是否正常";
+
+@interface MTTNetworkManagerConfig : NSObject
+
+@end
---
+++ MTTNetworkManger/MTTNetworkManger/Sources/MTTNetworkManagerConfig.m
@@ -0,0 +1,13 @@
+//
+//  MTTNetworkManagerConfig.m
+//  MTTNetworkManger
+//
+//  Created by 刘浩 on 2017/10/25.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+#import "MTTNetworkManagerConfig.h"
+
+@implementation MTTNetworkManagerConfig
+
+@end
